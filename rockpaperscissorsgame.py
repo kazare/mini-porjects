c525ef8251f1e901b148fe8ed481f992d835ec09
kazare <12612446+kazare@users.noreply.github.com>
2020-05-26 14:04:00 -0400

    Full playable version

--- rockpaperscissorsgame.py
+++ rockpaperscissorsgame.py
@@ -1,10 +1,13 @@
 # User will play rock, paper, scissors with the computer
 
-# delcaring variables
+# delcaring variables and importing random library
+import random
+
 rounds = 0
-maxRounds = 3
+maxRounds = 6
 userScore = 0
 cpuScore = 0
+options = ["rock", "paper", "scissors"]
 
 print('''
 
@@ -13,7 +16,7 @@ print('''
 	''' % maxRounds)
 
 while rounds < maxRounds:
-	cpu = "rock"
+	cpu = random.choice(options)
 	user = input("Please choose rock, paper, or scissors:")
 	rounds += 1
 
@@ -25,57 +28,75 @@ while rounds < maxRounds:
 		if user == "paper":
 			print("Congrats! You won!! Paper beats rock!")
 			userScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 		if user == "scissors":
 			print("You lost! Rock beats scissors")
 			cpuScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 		if user == "rock":
 			print("A tie!")
 			userScore += 1
 			cpuScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 
 	# if computer chooses paper
 	elif cpu == "paper":
 		if user == "scissors":
 			print("Congrats! You won!! Scissors beats paper!")
 			userScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 		if user == "rock":
 			print("You lost! Paper beats rock")
 			cpuScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 		if user == "paper":
 			print("A tie!")
 			userScore += 1
 			cpuScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 
 	# if computer chooses scissors
 	elif cpu == "scissors":
 		if user == "rock":
 			print("Congrats! You won!! Rock beats scissors!")
 			userScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 		if user == "paper":
-			print("You lost! Rock beats paper")
+			print("You lost! Scissors beats paper")
 			cpuScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 		if user == "scissors":
 			print("A tie!")
 			userScore += 1
 			cpuScore += 1
-			print("You're score: " + str(userScore))
-			print("Computer's score: " + str(cpuScore))
+			print("""
+				You're score: {0}
+				Computer's score: {1}
+				""".format(userScore, cpuScore))
 
 	if rounds == maxRounds:
 		print('''
